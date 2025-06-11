2fe361c929792621bcd02bf499060ed4a6031ae3
Antoni Wellisz <awwellisz@uchicago.edu>
2025-06-11 18:43:22 -0500

    added fake FSM for testing

--- src/largo/drivers/inserv.py
+++ src/largo/drivers/inserv.py
@@ -27,6 +27,7 @@ nspyre_init_logger(
 with InstrumentServer() as inserv:
     # local_inserv.add('subs', _HERE / 'subsystems_driver.py', 'SubsystemsDriver', args=[local_inserv, remote_gw], local_args=True)
     inserv.add('odmr_driver', _HERE / 'fake_odmr_driver.py', 'FakeODMRInstrument')
+    inserv.add('fsm_driver', _HERE / 'fake_fsm_driver.py', 'FakeFSM')
     # run a CLI (command-line interface) that allows the user to enter
     # commands to control the server
     serve_instrument_server_cli(inserv)
